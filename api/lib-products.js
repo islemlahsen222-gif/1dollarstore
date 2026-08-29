224e56e03ac1a7bb90f9d96930cc8179cd80dc89
islemlahsen222-gif <islemlahsen222@gmail.com>
2026-08-29 23:27:30 +0200

    Update cover image filename for 'The Power of the Page'

--- api/lib-products.js
+++ api/lib-products.js
@@ -43,7 +43,7 @@ const products = [
     title: { en: 'The Power of the Page', },
     description: { en: 'The Power of the Page is an inspiring and practical book about the power of writing, self-expression, and turning your ideas into clear goals. It helps readers organize their thoughts, discover their potential, and take meaningful steps toward personal growth.
 },
-     cover: '/The_Power_of_the_Page.png',
+     cover: '/cover-The_Power_of_the_Page.jpg',
     file: 'The_Power_of_the_Page.pdf'
    },
 ];
